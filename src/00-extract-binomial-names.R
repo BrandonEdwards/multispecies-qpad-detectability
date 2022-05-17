866780ef8344fd98369282c2b704dba1a539f533
Brandon Edwards <brandon.edwards144@gmail.com>
2022-05-17 13:16:20 -0400

    import bridtree taxonomy and match up families

--- src/00-extract-binomial-names.R
+++ src/00-extract-binomial-names.R
@@ -12,10 +12,11 @@
 
 #' This file comes directly from NA-POPS: analysis/01-combine-data.R
 #' which is not publicly available. Will be loaded in as "project_counts"
-load("data/counts.rda")
+load("data/raw/counts.rda")
 
-codes <- read.csv("util/IBP-Alpha-Codes20.csv")
-families <- read.csv("util/NACC_list_species.csv")
+codes <- read.csv("data/raw/IBP-Alpha-Codes20.csv")
+families <- read.csv("data/raw/NACC_list_species.csv")
+birdtree <- read.csv("data/raw/birdtree_taxonomy.csv")
 
 ####### Main Code #################################
 
@@ -23,6 +24,11 @@ families <- read.csv("util/NACC_list_species.csv")
 codes_red <- codes[which(codes$SPEC %in% unique(project_counts$Species)), ]
 codes_family <- merge(codes_red, families[, c("species", "family")], 
                       by.x = "SCINAME", by.y = "species")
+
+# Merge with BirdTree master taxonomy so we can use their scientific names
+birdtree_red <- birdtree[which(birdtree$English %in% codes_family$COMMONNAME), ]
+codes_family <- merge(codes_family, birdtree_red[, c("English", "Scientific")],
+                      by.x = "COMMONNAME", by.y = "English", all = TRUE)
 
 binomial <- codes_family[, c("SPEC", "COMMONNAME", "SCINAME", "family")]
 names(binomial) <- c("Code", "English", "Scientific", "Family")
@@ -33,6 +39,6 @@ binomial[which(binomial$Scientific == "Ixoreus naevius"), "Scientific"] <- "Zoot
 ####### Output ####################################
 
 write.table(binomial,
-            file = "output/binomial_names.csv",
+            file = "data/generated/binomial_names.csv",
             sep = ",",
             row.names = FALSE)
