cd857928e5d994e34144cc8201fe66100b4eb927
Brandon Edwards <brandon.edwards144@gmail.com>
2023-11-30 10:48:32 -0500

    testing other hummers

--- src/scripts/distance/6-distance-predictions.R
+++ src/scripts/distance/6-distance-predictions.R
@@ -26,27 +26,25 @@ n_chains <- 4
 refresh <- 10
 threads_per_chain <- 3
 
-distance_stan_data <- subset_distance_data(distance_stan_data = distance_stan_data,
+ruhu <- subset_distance_data(distance_stan_data = distance_stan_data,
                                            sps = "RUHU")
 
-distance_stan_data$grainsize <- 1
+alhu <- subset_distance_data(distance_stan_data = distance_stan_data,
+                             sps = "ALHU")
 
-inits <- generate_distance_inits(n_chains = n_chains,
-                                 napops_skip = c("BITH", "HASP", "KIWA", "LCTH", "LEPC", "SPOW"),
-                                 sp_list = distance_stan_data$sp_all,
-                                 param = "cp",
-                                 species_cp = distance_stan_data$species_cp,
-                                 species_ncp = distance_stan_data$species_ncp)
+rthu <- subset_distance_data(distance_stan_data = distance_stan_data,
+                             sps = "RTHU")
+
+bchu <- subset_distance_data(distance_stan_data = distance_stan_data,
+                             sps = "BCHU")
+
+distance_stan_data$grainsize <- 1
 
 distance_stan_data$sp_list <- NULL
 distance_stan_data$sp_all <- NULL
 
 # Scale the maximum distances for computational ease
-distance_stan_data$max_dist <- distance_stan_data$max_dist / 100
-
-# get rid of centre/scale attributes for modelling
-distance_stan_data$pitch <- distance_stan_data$pitch[,1]
-distance_stan_data$mass <- distance_stan_data$mass[,1]
+distance_stan_data$max_dist <- distance_stan_data$max_dist/10
 
 ####### Run Model #################################
 
@@ -63,20 +61,5 @@ stan_run <- ss_model$sample(
   threads_per_chain = threads_per_chain
 )
 
-
-
-
-
-
-
-
-
-
-
-
-model_file <- cmdstan_model(stan_file = "models/distance_cp.stan",
-                            cpp_options = list(stan_threads = TRUE))
-
-
-
-stan_run$save_object(file = paste0("output/model_runs/distance_predictions.RDS"))
+draws <- stan_run$sampler_diagnostics(format = "df")
+draws$log_tau <- stan_run$draws(variables = "log_tau[1]", format = "df")$`log_tau[1]`
